41518c963b67d4beb9226e8477807a51556c6492
RyomaJibiki <jibi2002ryou0924@gmail.com>
2025-07-05 16:05:07 +0900

    Update lcs.hpp

--- lcs.hpp
+++ lcs.hpp
@@ -2,7 +2,10 @@
 #pragma once // ヘッダーが複数回インクルードされるのを防ぐおまじない
 
 #include <string>
+#include <clocale>
 #include <vector>
+#include <locale>
+#include <codecvt>
 
 // LCS計算アルゴリズムの関数宣言
 std::string find_lcs_optimized(const std::string& s, const std::string& t);
